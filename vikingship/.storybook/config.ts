54ceec45863aacb7a73f370e1a870107b4427b45
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-18 21:00:24 -0800

    config storybook

--- vikingship/.storybook/config.ts
+++
@@ -1,6 +0,0 @@
-import {configure} from '@storybook/react'
-
-import "../src/styles/index.scss"
-
-// import all files ending with *.stories.js
-configure(require.context('../src', true, /\.stories\.tsx$/),module)
