eaad99da2bc45b66f43f793bb67243dcd8f70e92
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-19 20:55:38 -0800

    autoComplete show item

--- vikingship/src/components/AutoComplete/autoComplete.stories.tsx
+++ vikingship/src/components/AutoComplete/autoComplete.stories.tsx
@@ -3,6 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import AutoComplete from './autoComplete';
 
-const defaultAutoComplete = () => <AutoComplete />;
+const SimpleComplete = () => {
+  const options =["aaa", 'bbbbb','cccc', 'aaaddbb', 'bbbcccc'];
 
-storiesOf('AutoComplete component', module).add('AutoComplete Default UI', defaultAutoComplete);
+  const handleFetch = (query:string) =>{
+    return options.filter(name=>name.includes(query))
+  }
+
+  return (
+    <AutoComplete fetchSuggestions={handleFetch}/>
+  )
+};
+
+storiesOf('AutoComplete component', module)
+  .add('AutoComplete Simple', SimpleComplete);
--- vikingship/src/components/AutoComplete/autoComplete.tsx
+++ vikingship/src/components/AutoComplete/autoComplete.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {ChangeEvent, FC, useState} from 'react'
+//
+import Input, {InputProps} from '../Input/input';
 
-export default function AutoComplete() {
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+  fetchSuggestions: (str: string) => string[];
+  onSelect?:(item: string)=> void;
+  value?:string
+}
+
+
+const AutoComplete: FC<AutoCompleteProps> = (props) =>{
+  const {
+    fetchSuggestions,
+    onSelect,
+    value,
+    ...restProps
+  } = props;
+
+  const [inputValue, setInputValue] = useState(value);
+  const [suggestions, setSuiggestions] = useState<string[]>([]);
+
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{
+    const value = e.target.value.trim();
+    setInputValue(value);
+
+    if(value){
+      const result = fetchSuggestions(value);
+      setSuiggestions(result);
+    }
+    else
+    {
+      setSuiggestions([]);
+    }
+  }
+  
+  const generateDropDown = () =>{
+    return (
+      <ul>
+        {suggestions.map((item, index)=>{
+          return (
+              <li key={index}>{item}</li>
+          )
+        })}
+      </ul>
+    )
+  }
   return (
-    <div>
-      AutoComplete
+    <div className="viking-auto-complete">
+      <Input 
+        value={value}
+        onChange={handleChange}
+        {...restProps}
+      />
+      {(suggestions.length > 0 && generateDropDown())}
     </div>
   )
-}
+} 
+
+export default AutoComplete;
