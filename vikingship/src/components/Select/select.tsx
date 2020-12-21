e4832e8c2f38b39dfcd17ddaf6eae3dc7d772f66
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-20 17:48:14 -0800

    select component

--- vikingship/src/components/Select/select.tsx
+++ vikingship/src/components/Select/select.tsx
@@ -1,10 +1,18 @@
-import React, { FC, useState, createContext, useRef, useEffect } from 'react';
-
+import React, {
+  FC,
+  useState,
+  createContext,
+  useRef,
+  useEffect,
+  MouseEvent,
+} from 'react';
 import classNames from 'classnames';
 import Input from '../Input';
 import Icon from '../Icon';
 import useClickOutside from '../../hooks/useClickOutside';
 import Transition from '../Transition/transition';
+
+import { SelectOptionProps } from './option';
 
 export interface SelectProps {
   /**指定默认选中的条目	 可以是是字符串或者字符串数组*/
@@ -23,7 +31,7 @@ export interface SelectProps {
   onVisibleChange?: (visible: boolean) => void;
 }
 export interface ISelectContext {
-  onSelect?: (value: string, isSelected?: boolean) => void;
+  onSelect?: (value: string, isSelected: boolean) => void;
   selectedValues: string[];
   multiple?: boolean;
 }
@@ -52,8 +60,145 @@ const Select: FC<SelectProps> = (props) => {
     name,
     onChange,
     onVisibleChange,
+    children,
   } = props;
 
-  return <div></div>;
+  const input = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const containerWidth = useRef(0);
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+  const [menuOpen, setOpen] = useState(false);
+  const [value, setValue] = useState(defaultValue);
+
+  var handleOptionClick = function (value: string, isSelected: boolean) {
+    // update value
+    if (!multiple) {
+      setOpen(false);
+      setValue(value);
+      if (onVisibleChange) {
+        onVisibleChange(false);
+      }
+    } else {
+      setValue('');
+    }
+    var updatedValues = [value];
+    // click again to remove selected when is multiple mode
+    if (multiple) {
+      updatedValues = isSelected
+        ? selectedValues.filter(function (v) {
+            return v !== value;
+          })
+        : [...selectedValues, value];
+      setSelectedValues(updatedValues);
+    }
+    if (onChange) {
+      onChange(value, updatedValues);
+    }
+  };
+  useEffect(
+    function () {
+      // focus input
+      if (input.current) {
+        input.current.focus();
+        if (multiple && selectedValues.length > 0) {
+          input.current.placeholder = '';
+        } else {
+          if (placeholder) input.current.placeholder = placeholder;
+        }
+      }
+    },
+    [selectedValues, multiple, placeholder]
+  );
+  useEffect(function () {
+    if (containerRef.current) {
+      containerWidth.current = containerRef.current.getBoundingClientRect().width;
+    }
+  });
+  useClickOutside(containerRef, function () {
+    setOpen(false);
+    if (onVisibleChange && menuOpen) {
+      onVisibleChange(false);
+    }
+  });
+
+  const passedContext: ISelectContext = {
+    onSelect: handleOptionClick,
+    selectedValues: selectedValues,
+    multiple: multiple,
+  };
+
+  var handleClick = function (e: MouseEvent<HTMLElement>) {
+    e.preventDefault();
+    if (!disabled) {
+      setOpen(!menuOpen);
+      if (onVisibleChange) {
+        onVisibleChange(!menuOpen);
+      }
+    }
+  };
+  var generateOptions = function () {
+    return React.Children.map(children, function (child, i) {
+      var childElement = child as React.FunctionComponentElement<
+        SelectOptionProps
+      >;
+      if (childElement.type.displayName === 'Option') {
+        return React.cloneElement(childElement, {
+          index: 'select-' + i,
+        });
+      } else {
+        console.error(
+          'Warning: Select has a child which is not a Option component'
+        );
+      }
+    });
+  };
+  var containerClass = classNames('viking-select', {
+    'menu-is-open': menuOpen,
+    'is-disabled': disabled,
+    'is-multiple': multiple,
+  });
+  return (
+    <div className={containerClass} ref={containerRef}>
+      <div className="viking-select-input" onClick={handleClick}>
+        <Input
+          ref={input}
+          placeholder={placeholder}
+          value={value}
+          readOnly
+          icon="angle-down"
+          disabled={disabled}
+          name={name}
+        ></Input>
+        <SelectContext.Provider value={passedContext}>
+          <Transition in={menuOpen} animation="zoom-in-top" timeout={300}>
+            <ul className="viking-select-dropdown">{generateOptions()}</ul>
+          </Transition>
+          {multiple && (
+            <div
+              className="viking-selected-tags"
+              style={{ maxWidth: containerWidth.current - 32 }}
+            >
+              {selectedValues.map((value, index) => {
+                return (
+                  <span className="viking-tag" key={'tag-' + index}>
+                    <Icon
+                      icon="times"
+                      onClick={() => handleOptionClick(value, true)}
+                    />
+                  </span>
+                );
+              })}
+            </div>
+          )}
+        </SelectContext.Provider>
+      </div>
+    </div>
+  );
 };
+
+Select.defaultProps = {
+  name: 'viking-select',
+  placeholder: '请选择',
+};
+
 export default Select;
